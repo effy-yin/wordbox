4aafb3b20f76f4a4c1afc1943c6be18815f5e22f
zhangxi <zhangxi_1989@sina.com>
2014-01-09 16:36:49 +0800

    优化 _fillRect 方法

--- jquery.wordbox.js
+++ jquery.wordbox.js
@@ -1,7 +1,7 @@
 (function(window, $){
 
-    $.wordbox = function ($element, options) {
-        this.$element = $element
+    $.wordbox = function (options, element) {
+        this.$element = element
 
         if(!this._create(options)) {
             this.failed = true;
@@ -50,147 +50,22 @@
             else {
                 this.$element.width(this.$element.parent().width());
                 this.$element.height(this.$element.parent().height());
-            }           
+            }
 
-            this.fontSize = opts.fontSize ? opts.fontSize : this.$element.css("fontSize");
+            this.fontSize = opts.fontSize ? opts.fontSize : $element.css("fontSize");
 
-            this._fillRect(this.$element, this.words, opts);       
-            
-            this.$element.css("fontSize", this.fontSize);
+            this._fillRect(this.$element,
+                           this.words,
+                           0,
+                           this.words.length,
+						   0,
+						   0,
+                           parseFloat(this.$element.css("width")),
+                           parseFloat(this.$element.css("height"))
+                           );
 
+            //this._fillRect(this.$element, this.words,0,15);
             return true;
-        },
-
-        _fillRect: function(wrapper, words) {
-            var num = words.length,
-                wrapperW = $(wrapper).width(),
-                wrapperH = $(wrapper).height(),
-                ratio = wrapperW / wrapperH;
-
-            if(num == 0) {
-                return;
-            } else if(num == 1) {
-                $(wrapper).html(createBox({width: wrapperW, height: wrapperH, top: 0, left: 0, word: words[0], color: this._getNextColor()}));
-            } else if(ratio >= 2.5) {
-                var randCol = this._randRange(2, 1, 0.5),
-                    leftNum = Math.round(randCol[0] * num),
-                    rightNum = num - leftNum,
-                    leftWidth = Math.round(wrapperW * randCol[0]),                    
-                    rightWidth = wrapperW - leftWidth,
-                    html = '';
-
-                if(leftNum == 0) {
-                    leftNum = 1;
-                    rightNum--;
-                } else if(rightNum == 0) {
-                    rightNum = 1;
-                    leftNum--;
-                }
-
-                if(leftNum == 1) {
-                    html += this._createBox({width: leftWidth, height: wrapperH, top: 0, left: 0, word: words[0], color: this._getNextColor(), borderR: true});
-                } else {
-                    html += this._createBox({width: leftWidth, height: wrapperH, top: 0, left: 0, word: '', borderR: true});
-                }
-                if(rightNum == 1) {
-                    html += this._createBox({width: rightWidth, height: wrapperH, top: 0, left: leftWidth, word: words[num - 1], color: this._getNextColor()});
-                } else {
-                    html += this._createBox({width: rightWidth, height: wrapperH, top: 0, left: leftWidth, word: ''});
-                }
-                $(wrapper).html(html);
-
-                if(leftNum > 1) {
-                    var leftDiv = $(wrapper).children('div')[0];
-                    this._fillRect(leftDiv, words.slice(0, leftNum));
-                }
-                if(rightNum > 1) {
-                    var rightDiv = $(wrapper).children('div')[1];
-                    this._fillRect(rightDiv, words.slice(leftNum));
-                }
-            } else {
-                var randRow = this._randRange(2, 1, 0.5),
-                    topNum = Math.round(randRow[0] * num),
-                    bottomNum = num - topNum,
-                    topHeight = Math.round(wrapperH * randRow[0]),
-                    bottomHeight = wrapperH - topHeight,
-                    html = '';
-
-                if(topNum == 0) {
-                    topNum = 1;
-                    bottomNum--;
-                } else if(bottomNum == 0) {
-                    bottomNum = 1;
-                    topNum--;
-                }
-
-                if(topNum == 1) {
-                    html += this._createBox({width: wrapperW, height: topHeight, top: 0, left: 0, word: words[0], color: this._getNextColor(), borderB: true});
-                } else {
-                    html += this._createBox({width: wrapperW, height: topHeight, top: 0, left: 0, word: '', borderB: true});
-                }
-                if(bottomNum == 1) {
-                    html += this._createBox({width: wrapperW, height: bottomHeight, top: topHeight, left: 0, word: words[num - 1], color: this._getNextColor()});
-                } else {
-                    html += this._createBox({width: wrapperW, height: bottomHeight, top: topHeight, left: 0, word: ''});
-                }
-                $(wrapper).html(html);
-
-                if(topNum > 1) {
-                    var topDiv = $(wrapper).children('div')[0];
-                    this._fillRect(topDiv, words.slice(0, topNum));
-                }
-                if(bottomNum > 1) {
-                    var bottomDiv = $(wrapper).children('div')[1];
-                    this._fillRect(bottomDiv, words.slice(topNum));
-                }
-            }
-        },
-
-        _createBox: function(option) {
-            var width = option.borderR ? option.width - this.borderWidth : option.width,
-                height = option.borderB ? option.height - this.borderWidth : option.height,
-                lineHeight = height;            
-
-            if(option.word) {
-                var wordW = this._getWordsWidth(option.word.title);
-                if(wordW > width) {
-                    var line = Math.ceil(wordW / width);
-                    lineHeight = this.fontSize;
-                    var paddingTop = Math.max(0, (height - line * lineHeight) / 2);
-                    height -= paddingTop;
-                }
-                var html = '<div class="box" style="width:' + width + 'px;'
-                    + 'height:' + height + 'px;'
-                    + 'line-height:'+ lineHeight + 'px;'
-                    + 'top:' + option.top + 'px;'
-                    + 'left:' + option.left + 'px;'
-                    + (option.color ? ('background-color:' + option.color + ';') : '')
-                    + (option.borderR ? 'border-right:' + this.borderWidth + 'px solid #fff;' : '')
-                    + (option.borderB ? 'border-bottom:' + this.borderWidth + 'px solid #fff;' : '')
-                    + (paddingTop ? ('padding-top:' + paddingTop + 'px;') : '')
-                    + '">' + '<a href="'+option.word.url+'" >'+option.word.title + '</a></div>';
-            } else {
-                var html = '<div class="box" style="width:' + width + 'px;'
-                    + 'height:' + height + 'px;'               
-                    + 'top:' + option.top + 'px;'
-                    + 'left:' + option.left + 'px;'
-                    + (option.borderR ? 'border-right:' + this.borderWidth + 'px solid #fff;' : '')
-                    + (option.borderB ? 'border-bottom:' + this.borderWidth + 'px solid #fff;' : '')               
-                    + '"></div>';
-            }
-
-            return html;
-        },
-
-        _randRange: function(num, base, round) {        
-            if(num == 1) {
-                return [base];
-            }
-            var center = base / num,
-                min = center * (1 - round),
-                max = center * (1 + round),
-                rand = Math.random() * (max - min) + min;
-            return [rand].concat(this._randRange(num - 1, base - rand, round));
         },
 
         //随机排列数组元素，若priority为true，则返回数组arr前wordnum个元素
@@ -212,23 +87,122 @@
             }
         },
 
+     
+
+        /*
+        _fillRect
+        调整优化逻辑  by zhangxi http://zhangxi.me
+        原来有个小笨蛋写了一串if-else，80多行代码。
+        调整后一对半if-else，40行代码，实际创建box代码只在递归结束的一个地方调用
+        */
+        _fillRect: function(wrapper, words,left,right,x,y,width,height) 
+        {
+            if(right-left == 1)
+            {
+                //创建一个box
+                var div = this._createBox({width: width,
+                                          height: height,
+                                             top: y,
+                                            left: x,
+                                            word: words[left],
+                                           color: this._getNextColor(),
+                                         borderR: false});
+									   
+                $(wrapper).append(div);
+
+            }else if(left == right)
+            {
+				return;
+            }else
+            {
+                //随机分割线
+                var separator = this._separator(left,right);
+                //判断是垂直分割还是水平分割
+                var vertical = !((width/height) >=2.5);
+
+                if(vertical)
+                {
+                //垂直分割，按照比例分高度
+                var leftValue = (separator-left)/(right-left)*height;
+                var rightValue = height-leftValue;
+                
+                //继续分割上半部分
+                this._fillRect(wrapper,words,left,separator,x,y,width,leftValue);
+                //继续分割下半部分
+                this._fillRect(wrapper,words,separator,right,x,y+leftValue,width,rightValue);
+
+                }else
+				{
+				//水平分割，按照比例分宽度
+                var leftValue = (separator-left)/(right-left)*width;
+                var rightValue = width-leftValue;
+
+                this._fillRect(wrapper,words,left,separator,x,y,leftValue,height);
+                this._fillRect(wrapper,words,separator,right,x+leftValue,y,rightValue,height);
+                }
+            }
+        },
+
         _getNextColor: function() {
             var color = this.colors[this.colorPos % this.colors.length];
             this.colorPos++;
             return color;
         },
+        
+        _createBox: function(option) {
+            var width = option.borderR ? option.width - this.borderWidth : option.width,
+                height = option.borderB ? option.height - this.borderWidth : option.height,
+                lineHeight = height;            
+
+            if(option.word) {
+                var wordW = this._getWordsWidth(option.word.title);
+                if(wordW > width) {
+                    var line = Math.ceil(wordW / width);
+                    lineHeight = this.fontSize;
+                    var paddingTop = Math.max(0, (height - line * lineHeight) / 2);
+                }
+            }
+            
+            var html = '<div class="box" style="width:' + width + 'px;'
+                + 'height:' + height + 'px;'
+                + 'line-height:'+ lineHeight + 'px;'
+                + 'top:' + option.top + 'px;'
+                + 'left:' + option.left + 'px;'
+                + (option.color ? ('background-color:' + option.color + ';') : '')
+                + (option.borderR ? 'border-right:' + this.borderWidth + 'px solid #fff;' : '')
+                + (option.borderB ? 'border-bottom:' + this.borderWidth + 'px solid #fff;' : '')
+                + (paddingTop ? ('padding-top:' + paddingTop + 'px;') : '')
+                + '">' + '<a href="'+option.word.url+'" '+fontSize+' >'+option.word.title + '</a></div>';
+
+            return html;
+        },
 
         _getWordsWidth: function(word) {
             if($('#get_ww').size() < 1) {
-                $('<div id="get_ww" style="display:block;visibility:hidden;font-size:'+this.fontSize+'px"><span></span></div>').appendTo('body');
+                $('<div id="get_ww" style="display:block;visibility:hidden;"><span></span></div>').appendTo('body');
             }
             $('#get_ww span').html(word);
             return $('#get_ww span').width();
+        },
+
+        _separator: function(left, right) {        
+		
+            if(left == right) 
+			{
+			   return left;
+			}
+			else
+			{
+				var result = parseInt(Math.random() * (right - left)+ left);
+				if(result == left) result ++;
+				if(result == right) result --;
+				return result;
+			}
         }
     };
 
     $.fn.wordbox = function(options) {        
-        var instance = new $.wordbox(this, options);  
+        var instance = new $.wordbox(options, this);  
         if(instance.failed) {
             console.log("创建失败");
             return null;
@@ -242,7 +216,7 @@
                     window.fillTimeout = null;
                 }           
                 window.fillTimeout = setTimeout(function() {
-                    if(instance.$element.width() != instance.$element.parent().width() || instance.$element.height() != instance.$element.parent().height()) {
+                    if(instance.$element.width() != instance.$element.parent().width()) {
                         instance.$element.width(instance.$element.parent().width());
                         instance.$element.height(instance.$element.parent().height());
                         instance._fillRect(instance.$element, instance.words);
