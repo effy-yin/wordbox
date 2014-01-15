145b6d4dc1623137acc32edfdc22dca7f2c952a6
dodoroy <effy.yin@gmail.com>
2014-01-15 16:35:52 +0800

    逻辑优化

--- jquery.wordbox.js
+++ jquery.wordbox.js
@@ -1,7 +1,7 @@
 (function(window, $){
 
-   $.wordbox = function ($element, options) {
-     this.$element = $element
+    $.wordbox = function ($element, options) {
+        this.$element = $element;
 
         if(!this._create(options)) {
             this.failed = true;
@@ -13,30 +13,34 @@
         leadWord: null,
         words: null,
         colors: ['#cc5b34', '#c27c4d'],
-        borderWidth: 2,
         isFixedWidth: true,
         width: 1000,
-        height: 200,
-        fontSize: null  
+        height: 200
     };
 
     $.wordbox.prototype = {
+        /*
+         * this.defaults or $.wordbox.defaults ?? 
+         * defaults对象加在原型链上好还是加在$.wordbox上好？？
+         * defaults: {
 
+         * }, 
+        */
+       
         _create: function(options) {
             
             var opts = $.extend({}, $.wordbox.defaults, options);
+
             if(!this.$element || opts.words.length < 1) {
                 return false;
             }           
-            
+
             if(opts.isLead && opts.leadWord) {
                 this.words = [opts.leadWord].concat(this._randArray(opts.words, false));
             }
             else {
                 this.words = this._randArray(opts.words, false);
             }
-
-            this.borderWidth = opts.borderWidth;
 
             this.colors = opts.colors;  
             this.colorPos = 0;
@@ -50,159 +54,170 @@
             else {
                 this.$element.width(this.$element.parent().width());
                 this.$element.height(this.$element.parent().height());
-            }
+            }           
 
-            this.fontSize = opts.fontSize ? opts.fontSize : $element.css("fontSize");
+            this._fillRect(this.$element, 
+                            0, 
+                            0, 
+                            this.$element.width(), 
+                            this.$element.height(), 
+                            this.words);
 
-            this._fillRect(this.$element,
-                           this.words,
-                           0,
-                           this.words.length,
-						   0,
-						   0,
-                           parseFloat(this.$element.css("width")),
-                           parseFloat(this.$element.css("height"))
-                           );
-
-            //this._fillRect(this.$element, this.words,0,15);
             return true;
         },
 
-        //随机排列数组元素，若priority为true，则返回数组arr前wordnum个元素
-        _randArray: function(array, priority) {
-            var clone = array.slice();
-            if(priority) {
-                return clone;
-            } else {
-                var ret = [], rand;
-                for(var i = 0, len = array.length; i < len; i++) {
-                    rand = Math.floor(Math.random() * clone.length);
-                    tmp = clone[0];
-                    clone[0] = clone[rand];
-                    clone[rand] = tmp;
-                    ret.push(clone[0]);
-                    clone = clone.slice(1);
-                }
-                return ret;
+        /*
+         * 递归创建box
+         */
+        _fillRect: function(wrapper, left, top, width, height, words) {
+            var wordLen = words.length,               
+                ratio = width / height;
+
+            if(wordLen == 1) {
+                this._createBox(wrapper, 
+                                left, 
+                                top, 
+                                width, 
+                                height, 
+                                words[0], 
+                                this._getNextColor());
+                return;
+            } 
+
+            var dot = this._randRange(1, 2, 0.5),
+                wordLen1 = Math.round(wordLen * dot[0]),
+                wordLen2 = wordLen - wordLen1;
+            if(wordLen1 == 0) {
+                wordLen1 = 1;
+                wordLen2--;
+            } else if(wordLen2 == 0) {
+                wordLen2 = 1;
+                wordLen1--;
+            }
+
+            if(ratio >= 2.5) {
+                // 左右分割
+                var leftW = Math.round(width * dot[0]),                    
+                    rightW = width - leftW;
+
+                this._fillRect( wrapper, 
+                                left, 
+                                top, 
+                                leftW, 
+                                height, 
+                                words.slice(0, wordLen1));
+                this._fillRect( wrapper, 
+                                left+leftW, 
+                                top, 
+                                rightW, 
+                                height, 
+                                words.slice(wordLen1));             
+           } else {
+                // 上下分割
+                var topH = Math.round(height * dot[0]),
+                    bottomH = height - topH;
+
+                this._fillRect( wrapper, 
+                                left, 
+                                top, 
+                                width, 
+                                topH, 
+                                words.slice(0, wordLen1));
+                this._fillRect( wrapper, 
+                                left, 
+                                top+topH, 
+                                width, 
+                                height-topH, 
+                                words.slice(wordLen1));
             }
         },
-
-     
 
         /*
-        _fillRect
-        调整优化逻辑  by zhangxi http://zhangxi.me
-        原来有个小笨蛋写了一串if-else，80多行代码。
-        调整后一对半if-else，40行代码，实际创建box代码只在递归结束的一个地方调用
-        */
-        _fillRect: function(wrapper, words,left,right,x,y,width,height) 
-        {
-            if(right-left == 1)
-            {
-                //创建一个box
-                var div = this._createBox({width: width,
-                                          height: height,
-                                             top: y,
-                                            left: x,
-                                            word: words[left],
-                                           color: this._getNextColor(),
-                                         borderR: false});
-									   
-                $(wrapper).append(div);
+         * 函数功能：创建box
+         * 参数：left、right为box相对于wrapper绝对定位的偏移量
+         */
+        _createBox: function(wrapper, left, top, width, height, word, color) {
+            var lineHeight = height,
+                paddingTop = 0,        
+                wordW = this._getWordsWidth(word.title);
 
-            }else if(left == right)
-            {
-				return;
-            }else
-            {
-                //随机分割线
-                var separator = this._separator(left,right);
-                //判断是垂直分割还是水平分割
-                var vertical = !((width/height) >=2.5);
-
-                if(vertical)
-                {
-                //垂直分割，按照比例分高度
-                var leftValue = (separator-left)/(right-left)*height;
-                var rightValue = height-leftValue;
-                
-                //继续分割上半部分
-                this._fillRect(wrapper,words,left,separator,x,y,width,leftValue);
-                //继续分割下半部分
-                this._fillRect(wrapper,words,separator,right,x,y+leftValue,width,rightValue);
-
-                }else
-				{
-				//水平分割，按照比例分宽度
-                var leftValue = (separator-left)/(right-left)*width;
-                var rightValue = width-leftValue;
-
-                this._fillRect(wrapper,words,left,separator,x,y,leftValue,height);
-                this._fillRect(wrapper,words,separator,right,x+leftValue,y,rightValue,height);
-                }
+            // 如果box中文字的宽度超出box本身的宽度，则需要分多行显示
+            if(wordW > width) {
+                var line = Math.ceil(wordW / width);
+                // 注意设置line-height属性和padding-top属性
+                lineHeight = this.$element.css('font-size');
+                paddingTop = Math.max(0, (height - line * lineHeight) / 2);
+                height -= paddingTop;
             }
+            var html = '<div class="box" style="width:' + width + 'px;'
+                + 'height:' + height + 'px;'
+                + 'line-height:'+ lineHeight + 'px;'
+                + 'top:' + top + 'px;'
+                + 'left:' + left + 'px;'
+                + 'background-color:' + color + ';'                
+                + (paddingTop ? ('padding-top:' + paddingTop + 'px;') : '')
+                + '">' + '<a href="' + word.url + '" >' + word.title + '</a></div>';          
+
+            $(wrapper).append(html);
         },
+
+        /* 函数功能:将base随机分成num份
+         * base:    被分割的数
+         * num:     分割的份数         
+         * round:   base被分割之后两部分的最大差，为了避免每部分太大或太小
+         * return:  包含num个分界点的数组
+         */
+        _randRange: function(base, num, round) {        
+            if(num == 1) {
+                return [base];
+            }
+            var center = base / num,
+                min = center * (1 - round),
+                max = center * (1 + round),
+                rand = Math.random() * (max - min) + min;
+            return [rand].concat(this._randRange(base - rand, num - 1, round));
+        },
+
+        
 
         _getNextColor: function() {
             var color = this.colors[this.colorPos % this.colors.length];
             this.colorPos++;
             return color;
         },
-        
-        _createBox: function(option) {
-            var width = option.borderR ? option.width - this.borderWidth : option.width,
-                height = option.borderB ? option.height - this.borderWidth : option.height,
-                lineHeight = height;            
 
-            if(option.word) {
-                var wordW = this._getWordsWidth(option.word.title);
-                if(wordW > width) {
-                    var line = Math.ceil(wordW / width);
-                    lineHeight = this.fontSize;
-                    var paddingTop = Math.max(0, (height - line * lineHeight) / 2);
-                }
-            }
-            
-            var html = '<div class="box" style="width:' + width + 'px;'
-                + 'height:' + height + 'px;'
-                + 'line-height:'+ lineHeight + 'px;'
-                + 'top:' + option.top + 'px;'
-                + 'left:' + option.left + 'px;'
-                + (option.color ? ('background-color:' + option.color + ';') : '')
-                + (option.borderR ? 'border-right:' + this.borderWidth + 'px solid #fff;' : '')
-                + (option.borderB ? 'border-bottom:' + this.borderWidth + 'px solid #fff;' : '')
-                + (paddingTop ? ('padding-top:' + paddingTop + 'px;') : '')
-                + '">' + '<a href="'+option.word.url+'" '+fontSize+' >'+option.word.title + '</a></div>';
-
-            return html;
-        },
-
+        /*
+         * 获取指定字体大小的word的宽度
+         */
         _getWordsWidth: function(word) {
             if($('#get_ww').size() < 1) {
-               $('<div id="get_ww" style="display:block;visibility:hidden;font-size:'+this.fontSize+'px"><span></span></div>').appendTo('body');
+                $('<div id="get_ww" style="display:block;visibility:hidden;font-size:'+this.$element.css('font-size')+'px"><span></span></div>').appendTo('body');
             }
             $('#get_ww span').html(word);
             return $('#get_ww span').width();
         },
 
-        _separator: function(left, right) {        
-		
-            if(left == right) 
-			{
-			   return left;
-			}
-			else
-			{
-				var result = parseInt(Math.random() * (right - left)+ left);
-				if(result == left) result ++;
-				if(result == right) result --;
-				return result;
-			}
+        /*
+         * 随机排列数组元素
+         */
+        _randArray: function(array) {
+            var clone = array.slice(),          
+                ret = [], 
+                rand;
+            for(var i = 0, len = array.length; i < len; i++) {
+                rand = Math.floor(Math.random() * clone.length);
+                tmp = clone[0];
+                clone[0] = clone[rand];
+                clone[rand] = tmp;
+                ret.push(clone[0]);
+                clone = clone.slice(1);
+            }
+            return ret;
         }
     };
 
     $.fn.wordbox = function(options) {        
-        var instance = new $.wordbox(this, options); 
+        var instance = new $.wordbox(this, options);  
         if(instance.failed) {
             console.log("创建失败");
             return null;
@@ -225,6 +240,7 @@
                 }, 200);                 
             });
         }
+        // 返回jquery对象
         return this;
     };
 
